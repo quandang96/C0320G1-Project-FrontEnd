f80e49b8a4323fb25f397df4963590a9a04adf2d
thienng5898 <zeusfallen98@gmail.com>
2020-09-07 09:36:49 +0700

    pull D Toan

--- flight-agency-project/src/app/customer/customer-routing.module.ts
+++ flight-agency-project/src/app/customer/customer-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerInfoComponent} from './customer-info/customer-info.component';
+import {CustomerHomeComponent} from './customer-home/customer-home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CustomerHomeComponent, children: [
+      {path: 'info', component: CustomerInfoComponent }
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- flight-agency-project/src/app/customer/customer.module.ts
+++ flight-agency-project/src/app/customer/customer.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerInfoComponent } from './customer-info/customer-info.component';
+import { CustomerHomeComponent } from './customer-home/customer-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomerInfoComponent, CustomerHomeComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule
