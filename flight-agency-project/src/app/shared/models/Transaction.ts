68676a59d48c518863467668ff7ba5c02f167259
ngocthanh2992 <ngocthanh2992@gmail.com>
2020-09-07 11:36:59 +0700

    push

--- flight-agency-project/src/app/shared/models/Transaction.ts
+++ flight-agency-project/src/app/shared/models/Transaction.ts
@@ -1,7 +1,7 @@
 export interface Transaction {
   id: number;
-  flightSchedule: FlightSchedule;
-  passengers: Passenger[];
+  // flightSchedule: FlightSchedule;
+  // passengers: Passenger[];
   account: Account;
   price: number;
   createdTime: string;
