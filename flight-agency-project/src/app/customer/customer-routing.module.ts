7b2fdd85e181be0103143454504ef9fec37615d1
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-09-07 09:29:15 +0700

    Thêm component customer-home dùng chung cho trang customer

--- flight-agency-project/src/app/customer/customer-routing.module.ts
+++ flight-agency-project/src/app/customer/customer-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CustomerPaymentComponent} from './customer-payment/customer-payment.component';
+import {CustomerHomeComponent} from './customer-home/customer-home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CustomerHomeComponent, children: [
+      {path: 'payment', component: CustomerPaymentComponent}
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CustomerRoutingModule { }
+export class CustomerRoutingModule {
+}
--- flight-agency-project/src/app/customer/customer.module.ts
+++ flight-agency-project/src/app/customer/customer.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerPaymentComponent } from './customer-payment/customer-payment.component';
+import { CustomerHomeComponent } from './customer-home/customer-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomerPaymentComponent, CustomerHomeComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule
