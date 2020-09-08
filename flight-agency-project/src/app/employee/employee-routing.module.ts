8c85aa1bf8719cfac9b3f06f5871b67fdc4a6cb1
hothaodn <hothaodn@gmail.com>
2020-09-08 17:08:35 +0700

    B-Thao first push: promo component and service

--- flight-agency-project/src/app/employee/employee-routing.module.ts
+++ flight-agency-project/src/app/employee/employee-routing.module.ts
@@ -2,11 +2,14 @@ import { FindFlightComponent } from './find-flight/find-flight.component';
 import { BookTicketComponent } from './book-ticket/book-ticket.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {PromoListComponent} from './promo/promo-list/promo-list.component';
+import {PromoCreateComponent} from './promo/promo-create/promo-create.component';
 
 const routes: Routes = [
     {path:"bookTicket",component: BookTicketComponent},
-    {path:"findFlight",component:FindFlightComponent}
+    {path:"findFlight",component:FindFlightComponent},
+    {path:"promotion",component:PromoListComponent},
+    {path:"promotion/create",component:PromoCreateComponent}
 ];
 
 @NgModule({
---
+++ flight-agency-project/src/app/employee/promo/promo-create/promo-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-create',
+  templateUrl: './promo-create.component.html',
+  styleUrls: ['./promo-create.component.css']
+})
+export class PromoCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
