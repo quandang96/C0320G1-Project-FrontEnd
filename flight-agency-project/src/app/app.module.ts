0c5b8accf86a3beaf43f02573c5a330881e982fe
ngocthanh2992 <ngocthanh2992@gmail.com>
2020-09-08 08:31:41 +0700

    push

--- flight-agency-project/src/app/app.module.ts
+++ flight-agency-project/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './home/login/login.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SendFeedbackComponent } from './home/send-feedback/send-feedback.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
 
@@ -16,12 +19,15 @@ import {MaterialModule} from "./material.module";
     HomeComponent,
     LoginComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    SendFeedbackComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
     MaterialModule
   ],
   providers: [],
