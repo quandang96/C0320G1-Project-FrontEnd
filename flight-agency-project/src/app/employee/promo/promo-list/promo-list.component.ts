bec68358fd8a9695fa285ec0112577c84db54ee4
hothaodn <hothaodn@gmail.com>
2020-09-09 08:54:37 +0700

    Update prototype create

--- flight-agency-project/src/app/employee/promo/promo-list/promo-list.component.ts
+++ flight-agency-project/src/app/employee/promo/promo-list/promo-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {PromoService} from '../../../shared/services/promo.service';
+import {Observable} from 'rxjs';
+import {Airport} from '../../../shared/models/airport';
+import {Branch} from '../../../shared/models/branch';
+import {Promo} from '../../../shared/models/promo';
 
 @Component({
   selector: 'app-promo-list',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromoListComponent implements OnInit {
 
-  constructor() { }
+  private promoList: Observable<Promo>;
+  private airlineList: Observable<Branch>;
+  private airportList: Observable<Airport>;
+
+  constructor(
+    public promoService: PromoService
+  ) { }
 
   ngOnInit() {
+    this.promoService.getPromo('not-active').subscribe(data => {
+      this.promoList = data;
+    });
+    this.promoService.getAirlines().subscribe(data => {
+      this.airlineList = data;
+    });
+    this.promoService.getAirports().subscribe(data => {
+      this.airportList = data;
+    });
+    console.log('meo');
+    console.log(this.promoList);
   }
 
+  showPromoList(status: string) {
+    this.promoService.getPromo(status).subscribe(data => {
+      this.promoList = data;
+    });
+  }
 }
