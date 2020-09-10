1b5dd2b71851b024cee90868763ff7eb77cc35f6
hothaodn <hothaodn@gmail.com>
2020-09-10 08:53:07 +0700

    B-Thao fix model

--- flight-agency-project/src/app/shared/models/promo.ts
+++ flight-agency-project/src/app/shared/models/promo.ts
@@ -1,12 +1,15 @@
+import {Branch} from './branch';
+import {Airport} from './airport';
+
 export interface Promo {
   [prop: string]: any;
 
   id: number;
   namePromo: string;
   discount: number;
-  airline: string;
-  departurePlace: string;
-  arrivalPlace: string;
+  airline: Branch;
+  departurePlace: Airport;
+  arrivalPlace: Airport;
   promoDateStart: string;
   promoDateEnd: string;
   flightDepartureDateStart: string;
