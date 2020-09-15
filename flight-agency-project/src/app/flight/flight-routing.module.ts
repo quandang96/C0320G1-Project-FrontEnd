fde1b18f8502c39d04b70a89196516b59978e86b
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-09-15 08:25:18 +0700

    A-Duy update small change

--- flight-agency-project/src/app/flight/flight-routing.module.ts
+++ flight-agency-project/src/app/flight/flight-routing.module.ts
@@ -4,14 +4,20 @@ import { FlightCenterComponent } from './flight-center/flight-center.component';
 import { FlightScheduleListComponent } from './flight-schedule-list/flight-schedule-list.component';
 import { AirportResolverService } from './airport-resolver.service';
 import { FlightPassengerInfoComponent } from './flight-passenger-info/flight-passenger-info.component';
+import { RoleGuard } from '../guards/role.guard';
+import { FlightGuardGuard } from './flight-guard.guard';
 
 
 const routes: Routes = [{
   path: '',
   component: FlightCenterComponent,
+  data: {
+    expectedRole: 'ROLE_USER'
+  },
+  canActivate: [RoleGuard],
   children: [
     { path: 'schedule', component: FlightScheduleListComponent, resolve: { airports: AirportResolverService } },
-    { path: 'confirm', component: FlightPassengerInfoComponent }
+    { path: 'confirm', component: FlightPassengerInfoComponent, canActivate: [FlightGuardGuard] }
   ]
 }];
 
