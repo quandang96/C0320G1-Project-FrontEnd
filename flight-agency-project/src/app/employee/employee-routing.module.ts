4841ee9e867caeb1775ee0bb8db621b1231c8ee3
quandang1996 <quandanght@gmail.com>
2020-09-15 09:59:34 +0700

    merge B-Hung

--- flight-agency-project/src/app/employee/employee-routing.module.ts
+++ flight-agency-project/src/app/employee/employee-routing.module.ts
@@ -3,12 +3,28 @@ import { FindFlightComponent } from './find-flight/find-flight.component';
 import { BookTicketComponent } from './book-ticket/book-ticket.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoleGuard } from '../guards/role.guard';
 
 
 const routes: Routes = [
-    {path:"bookTicket",component: BookTicketComponent},
-    {path:"findFlight",component:FindFlightComponent},
-    {path: "transaction/invoice/:id",component: InvoiceTicketComponent}
+  {
+    path: "bookTicket", component: BookTicketComponent, canActivate: [RoleGuard],
+    data: {
+      expectedRole: 'ROLE_EMPLOYEE'
+    }
+  },
+  {
+    path: "findFlight", component: FindFlightComponent, canActivate: [RoleGuard],
+    data: {
+      expectedRole: 'ROLE_EMPLOYEE'
+    }
+  },
+  {
+    path: "transaction/invoice/:id", component: InvoiceTicketComponent, canActivate: [RoleGuard],
+    data: {
+      expectedRole: 'ROLE_EMPLOYEE'
+    }
+  }
 ];
 
 @NgModule({
