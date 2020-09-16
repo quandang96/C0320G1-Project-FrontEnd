be7f8d724ee3ab0c3eacb716165e10242f22e54a
quandang1996 <quandanght@gmail.com>
2020-09-16 14:36:09 +0700

    merge thao

--- flight-agency-project/src/app/shared/models/dto/employeeDto.ts
+++ flight-agency-project/src/app/shared/models/dto/employeeDto.ts
@@ -1,18 +1,3 @@
-// tslint:disable-next-line:class-name
-export class employeeDto {
-  id: number;
-  fullName: string;
-  birthDate: string;
-  gender: string;
-  email: string;
-  phoneNumber: string;
-  address: string;
-  avatarUrl: string;
-  password: string;
-  newPassword: string;
-  confirmPassword: string;
-  backendMessage: string[];
-}
 
 export interface EmployeeDTO {
   id?: number;
