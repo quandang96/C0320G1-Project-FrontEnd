005154c4d12037294a9458f9ed241bb2059e74ba
quandang1996 <quandanght@gmail.com>
2020-09-16 17:02:07 +0700

    Sua B-Hung

--- flight-agency-project/src/app/employee/book-ticket/book-ticket-step2/book-ticket-step2.component.ts
+++ flight-agency-project/src/app/employee/book-ticket/book-ticket-step2/book-ticket-step2.component.ts
@@ -22,113 +22,113 @@ export class BookTicketStep2Component implements OnInit {
 
   @Input() flight = {} as EmployeeFlightSearchDTO;
   //Nhận biến flightID
-  flightIds : number[];
+  flightIds: number[];
   @Input() ticketForm: FormGroup;
-  departureFlight={} as FlightSchedule;
-  arrivalFlight={} as FlightSchedule;
+  departureFlight = {} as FlightSchedule;
+  arrivalFlight = {} as FlightSchedule;
   customer = {} as Account;
-  emailCheck:string = "";
-  totalPrice: number =0;
-  transactionPassengerDTO ={} as TransactionPassengerDTO;
-  passengers =[] as EmployeePassengerDTO[];
+  emailCheck: string = "";
+  totalPrice: number = 0;
+  transactionPassengerDTO = {} as TransactionPassengerDTO;
+  passengers = [] as EmployeePassengerDTO[];
   transactions = [] as EmployeeTransactionDTO[];
-  deptTransaction={} as EmployeeTransactionDTO;
-  arvTransaction={} as EmployeeTransactionDTO;
+  deptTransaction = {} as EmployeeTransactionDTO;
+  arvTransaction = {} as EmployeeTransactionDTO;
   saveTransactions = [] as Transaction[];
   //Khai báo formArray
   childPassengers: FormArray;
   adultPassengers: FormArray;
-  check : Boolean = false;
-  
-  constructor(private data:DataService,
-              private employeeService:EmployeeService,
-              private router:Router) { }
+  check: Boolean = false;
+
+  constructor(private data: DataService,
+    private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit() {
 
     this.childPassengers = this.ticketForm.get('childPassengers') as FormArray;
     this.adultPassengers = this.ticketForm.get('adultPassengers') as FormArray;
-    this.data.currentMessage.subscribe(data=>{
-      this.flightIds=data;
-      if(this.flightIds.length!=0){
-        this.employeeService.findFlightById(this.flightIds[0]).subscribe(data=>{
-          this.departureFlight=data;
-          if(this.flightIds.length!=1){
-            this.employeeService.findFlightById(this.flightIds[1]).subscribe(data=>{
+    this.data.currentMessage.subscribe(data => {
+      this.flightIds = data;
+      if (this.flightIds.length != 0) {
+        this.employeeService.findFlightById(this.flightIds[0]).subscribe(data => {
+          this.departureFlight = data;
+          if (this.flightIds.length != 1) {
+            this.employeeService.findFlightById(this.flightIds[1]).subscribe(data => {
               this.arrivalFlight = data;
             })
           }
         })
       }
     })
-}
+  }
 
   //lặp số lượng
-  counter(i:number){
+  counter(i: number) {
     return new Array(i);
   }
 
   //Hàm kiểm tra email
-  checkEmail(){
-    this.emailCheck=""
+  checkEmail() {
+    this.emailCheck = ""
     this.emailCheck = this.ticketForm.get('otherDetails').get('emailCheck').value;
-    this.employeeService.checkAccountByEmail(this.emailCheck).subscribe(data=>{
-      this.customer= data;
-      this.emailCheck="Email hợp lệ";
-    },error=>{
+    this.employeeService.checkAccountByEmail(this.emailCheck).subscribe(data => {
+      this.customer = data;
+      this.emailCheck = "Email hợp lệ";
+    }, error => {
       this.emailCheck = "Không tìm thấy người dùng";
     })
   }
 
   //Cập nhật tổng tiền hành lý chiều đi
-  getTotalDeptLuggagePrice():number{
+  getTotalDeptLuggagePrice(): number {
     let luggagePrice = 0;
     let adultPassengers = this.ticketForm.get("adultPassengers") as FormArray;
     let childPassengers = this.ticketForm.get("childPassengers") as FormArray;
-    for(let i=0;i<this.flight.adult;i++){
-      luggagePrice+=adultPassengers.at(i).get('deptLuggagePrice').value*1;
+    for (let i = 0; i < this.flight.adult; i++) {
+      luggagePrice += adultPassengers.at(i).get('deptLuggagePrice').value * 1;
     }
-    for(let i=0;i<this.flight.child;i++){
-      luggagePrice+=childPassengers.at(i).get('deptLuggagePrice').value*1;
+    for (let i = 0; i < this.flight.child; i++) {
+      luggagePrice += childPassengers.at(i).get('deptLuggagePrice').value * 1;
     }
     return luggagePrice;
   }
 
   //Cập nhật tổng tiền hành lý chiều về
-  getTotalArvLuggagePrice(): number{
+  getTotalArvLuggagePrice(): number {
     let luggagePrice = 0;
     let adultPassengers = this.ticketForm.get("adultPassengers") as FormArray;
     let childPassengers = this.ticketForm.get("childPassengers") as FormArray;
-    for(let i=0;i<this.flight.adult;i++){
-      luggagePrice+=adultPassengers.at(i).get('arvLuggagePrice').value*1;
+    for (let i = 0; i < this.flight.adult; i++) {
+      luggagePrice += adultPassengers.at(i).get('arvLuggagePrice').value * 1;
     }
-    for(let i=0;i<this.flight.child;i++){
-      luggagePrice+=childPassengers.at(i).get('arvLuggagePrice').value*1;
+    for (let i = 0; i < this.flight.child; i++) {
+      luggagePrice += childPassengers.at(i).get('arvLuggagePrice').value * 1;
     }
     return luggagePrice;
   }
 
   //cập nhật totalPrice
-  changeTotalPrice(){
+  changeTotalPrice() {
     let deptPrice = this.ticketForm.get('otherDetails').get('deptPrice').value;
     let arvPrice = this.ticketForm.get('otherDetails').get('arvPrice').value;
-    let luggagePrice = this.getTotalArvLuggagePrice()+this.getTotalDeptLuggagePrice();
-    this.totalPrice= Number.parseInt(deptPrice)+Number.parseInt(arvPrice)+luggagePrice;
+    let luggagePrice = this.getTotalArvLuggagePrice() + this.getTotalDeptLuggagePrice();
+    this.totalPrice = Number.parseInt(deptPrice) + Number.parseInt(arvPrice) + luggagePrice;
   }
 
 
   //Lưu vé
-  async saveTicket(){
+  async saveTicket() {
     this.passengers = [];
     this.transactions = [];
     //Lưu khách hàng
     let passenger = {} as EmployeePassengerDTO;
     let adultPassengers = this.ticketForm.get("adultPassengers") as FormArray;
     let childPassengers = this.ticketForm.get("childPassengers") as FormArray;
-    for(let i=0;i<this.flight.adult;i++){
-      passenger={
+    for (let i = 0; i < this.flight.adult; i++) {
+      passenger = {
         id: null,
-        fullName : adultPassengers.at(i).get("fullName").value,
+        fullName: adultPassengers.at(i).get("fullName").value,
         identifierCard: adultPassengers.at(i).get("identifierCard").value,
         email: adultPassengers.at(i).get("email").value,
         phoneNumber: adultPassengers.at(i).get("phoneNumber").value,
@@ -139,11 +139,11 @@ export class BookTicketStep2Component implements OnInit {
       }
       this.passengers.push(passenger);
     }
-    
-    for(let i=0;i<this.flight.child;i++){
-      passenger={
+
+    for (let i = 0; i < this.flight.child; i++) {
+      passenger = {
         id: null,
-        fullName : childPassengers.at(i).get("fullName").value,
+        fullName: childPassengers.at(i).get("fullName").value,
         identifierCard: null,
         email: null,
         phoneNumber: childPassengers.at(i).get("phoneNumber").value,
@@ -154,18 +154,18 @@ export class BookTicketStep2Component implements OnInit {
       }
       this.passengers.push(passenger);
     }
-    
+
     //Lưu transaction
     //Chiều đi
     let createdTime = new DatePipe('vi-VN').transform(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", 'GMT+7');
     let due = new Date();
-    due.setHours(due.getHours()+2); 
+    due.setHours(due.getHours() + 2);
     let dueTime = new DatePipe('vi-VN').transform(due, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", 'GMT+7');
     this.deptTransaction = {
       id: null,
       createdTime: createdTime,
       dueTime: dueTime,
-      price: this.arrivalFlight?this.ticketForm.get('otherDetails').get('deptPrice').value*1+this.getTotalDeptLuggagePrice() : this.totalPrice,
+      price: this.arrivalFlight ? this.ticketForm.get('otherDetails').get('deptPrice').value * 1 + this.getTotalDeptLuggagePrice() : this.totalPrice,
       status: "Chờ thanh toán",
       account: this.customer as Account,
       flightSchedule: this.departureFlight,
@@ -173,12 +173,12 @@ export class BookTicketStep2Component implements OnInit {
     this.transactions.push(this.deptTransaction);
 
     //Lưu chiều về
-    if(this.flightIds.length!=1){
+    if (this.flightIds.length != 1) {
       this.arvTransaction = {
         id: null,
         createdTime: createdTime,
         dueTime: dueTime,
-        price: this.ticketForm.get('otherDetails').get('arvPrice').value*1 + this.getTotalArvLuggagePrice(),
+        price: this.ticketForm.get('otherDetails').get('arvPrice').value * 1 + this.getTotalArvLuggagePrice(),
         status: "Chờ thanh toán",
         account: this.customer as Account,
         flightSchedule: this.arrivalFlight,
@@ -189,21 +189,21 @@ export class BookTicketStep2Component implements OnInit {
       transactions: this.transactions,
       passengers: this.passengers
     }
-    this.employeeService.saveTransactionAndPassenger(this.transactionPassengerDTO).subscribe(data=>{
+    this.employeeService.saveTransactionAndPassenger(this.transactionPassengerDTO).subscribe(data => {
       this.saveTransactions = data;
-      if (this.check == true) {        
-        for(let tran of this.saveTransactions){
+      if (this.check == true) {
+        for (let tran of this.saveTransactions) {
           console.log(tran)
-          window.open("/employee/transaction/invoice/"+ tran.id,"_blank");
+          window.open("/employee/transaction/invoice/" + tran.id, "_blank");
         }
-      } 
-      window.location.href="/employee";  
+      }
+      window.location.href = "/employee/findFlight";
     })
   }
 
   //router
-  goToInvoicePage(){    
-   this.check=true;
-   this.saveTicket();
+  goToInvoicePage() {
+    this.check = true;
+    this.saveTicket();
   }
 }
